6cec0c0492f965853f709e3e6058a1830d78800f
schefco <c.scheffler988@gmail.com>
2026-04-28 10:52:32 -0400

    Updated appsettings.json

--- Schefco.TaskFlow.API/obj/Debug/net8.0/Schefco.TaskFlow.API.AssemblyInfo.cs
+++ Schefco.TaskFlow.API/obj/Debug/net8.0/Schefco.TaskFlow.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Schefco.TaskFlow.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a8c8b2bb3c68f7f86b1a2d3e59fd18a3d36b50a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca323e8ec0aa23c7a741bcf017d8b6078f6f295b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Schefco.TaskFlow.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Schefco.TaskFlow.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
